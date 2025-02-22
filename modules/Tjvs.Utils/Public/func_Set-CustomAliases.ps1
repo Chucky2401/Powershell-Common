c41d4737b52e85a2fdaa3919de98d04eef0d39fd
The Black Wizard <chuckydu24@gmail.com>
2025-02-22 17:23:27 +0100

    feat(Set-CustomAliases): add 'y' for Yazi

--- modules/Tjvs.Utils/Public/func_Set-CustomAliases.ps1
+++ modules/Tjvs.Utils/Public/func_Set-CustomAliases.ps1
@@ -64,4 +64,15 @@ function Set-CustomAliases {
     }
 
     Set-Alias -Name lg -Value lazygit -Scope Global
+
+    # Yazi
+    function global:y {
+        $tmp = [System.IO.Path]::GetTempFileName()
+        yazi $args --cwd-file="$tmp"
+        $cwd = Get-Content -Path $tmp -Encoding UTF8
+        if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+            Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd))
+        }
+        Remove-Item -Path $tmp
+    }
 }
